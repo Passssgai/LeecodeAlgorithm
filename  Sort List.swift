15ac7027e99fea2faf24be4fae19fd96c83d96c4
gaizuojia <gaizuojia@gmail.com>
2022-03-04 21:16:56 +0800

    链表排序需要继续修改

---
+++  Sort List.swift
@@ -0,0 +1,68 @@
+/*
+给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+
+ 
+
+示例 1：
+
+
+输入：head = [4,2,1,3]
+输出：[1,2,3,4]
+示例 2：
+
+
+输入：head = [-1,5,3,4,0]
+输出：[-1,0,3,4,5]
+示例 3：
+
+输入：head = []
+输出：[]
+ 
+
+提示：
+
+链表中节点的数目在范围 [0, 5 * 104] 内
+-105 <= Node.val <= 105
+ 
+
+进阶：你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sort-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+class Solution {
+    func sortList(_ head: ListNode?) -> ListNode? {
+        guard let head = head else {
+            return nil
+        }
+        while head?.next != nil {
+            var slow = head
+            var fast = head
+            while fast?.next != nil && fast?.next?.next != nil {
+                slow = slow?.next
+                fast = fast?.next?.next
+            }
+            var next = slow?.next
+            slow?.next = nil
+            slow = next
+            var pre = head
+            var cur = next
+            while cur != nil {
+                if cur!.val < pre!.val {
+                    let temp = cur!.next
+                    cur!.next = pre
+                    pre = cur
+                    cur = temp
+                } else {
+                    let temp = cur!.next
+                    cur!.next = pre?.next
+                    pre?.next = cur
+                    cur = temp
+                }
+            }
+            head = pre
+        }
+        return head
+    }
+}
