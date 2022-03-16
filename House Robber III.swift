e1396437eaa079a7737a6fd10b9367bd5ab4d3dd
gaizuojia <gaizuojia@gmail.com>
2022-03-16 23:18:54 +0800

    树的经典DP问题

---
+++ House Robber III.swift
@@ -0,0 +1,69 @@
+
+/*
+小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为 root 。
+
+除了 root 之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果 两个直接相连的房子在同一天晚上被打劫 ，房屋将自动报警。
+
+给定二叉树的 root 。返回 在不触动警报的情况下 ，小偷能够盗取的最高金额 。
+
+ 
+
+示例 1:
+
+
+
+输入: root = [3,2,3,null,3,null,1]
+输出: 7 
+解释: 小偷一晚能够盗取的最高金额 3 + 3 + 1 = 7
+示例 2:
+
+
+
+输入: root = [3,4,5,1,3,null,1]
+输出: 9
+解释: 小偷一晚能够盗取的最高金额 4 + 5 = 9
+ 
+
+提示：
+
+树的节点数在 [1, 104] 范围内
+0 <= Node.val <= 104
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/house-robber-iii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func rob(_ root: TreeNode?) -> Int {
+        var maxAmount = Int.min
+        func dfs(_ root:TreeNode?) ->(Int,Int) {
+            guard let root = root else {
+                return (0,0)
+            }
+            let left = dfs(root.left)
+            let right = dfs(root.right)
+            let selected = root.val + left.1 + right.1
+            let noselected = max(left.0, left.1) + max(right.0, right.1)
+            return (selected,noselected)
+        }
+        let rootStatus = dfs(root)
+        maxAmount = max(rootStatus.0, rootStatus.1)
+        return maxAmount
+    }
+}
